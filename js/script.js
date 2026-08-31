2f3fa6e7b2d327dea4f65dd8d8ea28c768d83bd1
Joel Alvarez <Joel124@outlook.es>
2026-08-31 05:11:03 -0400

    refactor: query primary navigation by id

--- js/script.js
+++ js/script.js
@@ -48,7 +48,7 @@ function copyCode() {
 
 document.addEventListener("DOMContentLoaded", function () {
   const mobileMenuButton = document.querySelector(".mobile-menu-icon");
-  const nav = document.querySelector(".header__nav");
+  const nav = document.getElementById("primary-navigation");
   const copyButton = document.querySelector(".copy-icon");
   const tabs = document.querySelectorAll(".code-container__tab");
   const codeBlocks = document.querySelectorAll(".code-container__code");
